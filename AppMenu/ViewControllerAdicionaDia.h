7110de367895f6e0168d524f21a3da33756b39f6
denilson100 <denilson100@gmail.com>
2016-07-12 10:48:00 -0300

    Função adiciona dia implementada.

--- AppMenu/ViewControllerAdicionaDia.h
+++ AppMenu/ViewControllerAdicionaDia.h
@@ -10,4 +10,40 @@
 
 @interface ViewControllerAdicionaDia : ViewController
 
+
+@property (weak, nonatomic) IBOutlet UITextField *textFieldDia;
+
+
+@property (weak, nonatomic) IBOutlet UITextField *textFieldMes;
+
+
+
+@property (weak, nonatomic) IBOutlet UITextField *textFieldAno;
+
+
+- (IBAction)addDia:(UIButton *)sender;
+
+
+- (IBAction)addMes:(UIButton *)sender;
+
+
+- (IBAction)addAno:(UIButton *)sender;
+
+
+- (IBAction)removeDia:(UIButton *)sender;
+
+
+- (IBAction)removeMes:(UIButton *)sender;
+
+
+- (IBAction)removeAno:(UIButton *)sender;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *lbDia;
+
+@property (weak, nonatomic) IBOutlet UILabel *lbMes;
+
+@property (weak, nonatomic) IBOutlet UILabel *lbAno;
+
+
 @end
--- AppMenu/ViewControllerAdicionaDia.m
+++ AppMenu/ViewControllerAdicionaDia.m
@@ -12,26 +12,146 @@
 
 @end
 
-@implementation ViewControllerAdicionaDia
+@implementation ViewControllerAdicionaDia 
+    
+    int dia = 12;
+    int mes = 7;
+    int ano = 2016;
+
+    int valorDia;
+    int valorMes;
+    int valorAno;
+
+    int diaInterno = 0;  //variavel global para não zerar
+    int mesInterno = 0;
+    int anoInterno = 0;
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    diaInterno = dia;   //Uma var para contar o loop e zerar.
+    mesInterno = mes;
+    
+    self.lbDia.text = [NSString stringWithFormat:@"%d", dia];
+    self.lbMes.text = [NSString stringWithFormat:@"%d", mes];
+    self.lbAno.text = [NSString stringWithFormat:@"%d", ano];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+
+- (IBAction)addDia:(UIButton *)sender {
+    
+    valorDia = self.textFieldDia.text.integerValue;
+    
+    int somaDosDias = (dia + valorDia) - 1;    //Pega atual e valor digitado
+    
+    //Pega o num de dias na tela e soma com o dia digitado.
+    //Entra no if e a cada 30 dias incrementa o mes.
+    //Entra no proximo if e a cada 12 meses incrementa ano.
+    while (dia <= somaDosDias) {
+        
+        if (dia % 30 == 0) {
+            diaInterno = 0;
+            mes++;
+            mesInterno++;
+            
+            if (mes % 13 == 0) {
+                mesInterno = 1;
+                ano++;
+            }
+        }
+        diaInterno++;
+        dia++;
+    }
+    self.lbMes.text = [NSString stringWithFormat:@"%d", mesInterno];
+    self.lbDia.text = [NSString stringWithFormat:@"%d", diaInterno];
+    self.lbAno.text = [NSString stringWithFormat:@"%d", ano];
+    self.textFieldDia.text = @"";
+    self.textFieldMes.text = @"";
+    self.textFieldAno.text = @"";
 }
 
-/*
-#pragma mark - Navigation
+- (IBAction)addMes:(UIButton *)sender {
+    
+    valorDia = self.textFieldMes.text.integerValue;
+    valorDia = valorDia * 30;                       //Multiplica por 30 para formar 1 mes
+    
+    int somaDosDias = (dia + valorDia) - 1;         //Pega atual e valor digitado
+    
+    while (dia <= somaDosDias) {
+        
+        if (dia % 30 == 0) {
+            diaInterno = 0;
+            mes++;
+            mesInterno++;
+            
+            if (mes % 12 == 0) {
+                mesInterno = 1;
+                ano++;
+            }
+        }
+        diaInterno++;
+        dia++;
+    }
+    self.lbMes.text = [NSString stringWithFormat:@"%d", mesInterno];
+    self.lbDia.text = [NSString stringWithFormat:@"%d", diaInterno];
+    self.lbAno.text = [NSString stringWithFormat:@"%d", ano];
+    self.textFieldDia.text = @"";
+    self.textFieldMes.text = @"";
+    self.textFieldAno.text = @"";
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
-*/
 
+- (IBAction)addAno:(UIButton *)sender {
+    
+    
+    ano = ano + self.textFieldAno.text.integerValue;
+    
+    self.lbMes.text = [NSString stringWithFormat:@"%d", mesInterno];
+    self.lbDia.text = [NSString stringWithFormat:@"%d", diaInterno];
+    self.lbAno.text = [NSString stringWithFormat:@"%d", ano];
+    self.textFieldDia.text = @"";
+    self.textFieldMes.text = @"";
+    self.textFieldAno.text = @"";
+}
+
+- (IBAction)removeDia:(UIButton *)sender {
+    
+    valorDia = self.textFieldDia.text.integerValue;
+    
+    int somaDosDias = (dia + valorDia) - 1;    //Pega atual e valor digitado
+    
+    //Pega o num de dias na tela e soma com o dia digitado.
+    //Entra no if e a cada 30 dias incrementa o mes.
+    //Entra no proximo if e a cada 12 meses incrementa ano.
+    while (dia >= somaDosDias) {
+        
+        if (dia < 1) {
+            diaInterno = 31;
+            mes--;
+            mesInterno--;
+            
+            if (mes < 1) {
+                mesInterno = 13;
+                ano--;
+            }
+        }
+        diaInterno--;
+        dia--;
+    }
+    self.lbMes.text = [NSString stringWithFormat:@"%d", mesInterno];
+    self.lbDia.text = [NSString stringWithFormat:@"%d", diaInterno];
+    self.lbAno.text = [NSString stringWithFormat:@"%d", ano];
+    self.textFieldDia.text = @"";
+    self.textFieldMes.text = @"";
+    self.textFieldAno.text = @"";
+}
+
+- (IBAction)removeMes:(UIButton *)sender {
+}
+
+- (IBAction)removeAno:(UIButton *)sender {
+}
 @end
